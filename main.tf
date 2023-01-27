0bd828f68b8be51f7b8b2a0d6545d8581490cee8
RaajAnkar1989 <RaajAnkar1989@gmail.com>
2023-01-27 22:08:05 +0000

    new

--- main.tf
+++ main.tf
@@ -9,14 +9,14 @@ resource "aws_internet_gateway" "test-igw" {
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.Test-vpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-west-2a"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "private" {
   vpc_id                  = aws_vpc.Test-vpc.id
   cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-west-2b"
+  availability_zone       = "us-east-1a"
 }
 
 resource "aws_route_table" "public" {
